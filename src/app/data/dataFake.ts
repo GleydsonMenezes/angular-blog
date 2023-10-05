5ae6d86e0fb45f7fba802a05f9e60e1b5cdf693b
Gleydson Menezes <gsmenezes996@gmail.com>
2023-10-05 13:55:24 -0300

    dataFake created, Content.componentts updated

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":1,
+    "title":"Steam estimula o desenvolvimento de jogos Indies",
+    "description":"Devido a explosão de vendas do SteamDeck, Gabe Newell, fundador da Steam (loja digital de jogos do PC) tem estimulado a produção de jogos Indies",
+    "photoCover":"https://cdn.ome.lt/zIa7Z-hUL8c4jUdtT6jwfEXTZOY=/1200x630/smart/extras/conteudos/Steam_Deck_header.jpg"
+  },
+  {
+    "id":2,
+    "title":"GC Studio passará a adotar o nome de CyberGhost Studio",
+    "description":"Devido a um reposicionamento de marca, a GC Studio agora será a CyberGhost Studio. A promessa é de uma especialização com"+
+    "com temáticas futuristicas e sobrenaturais em jogos de Top-down Shooter",
+    "photoCover":"https://dropsdejogos.uai.com.br/wp-content/uploads/sites/10/2022/04/reproducao-indiegames-divulgacao-950x534.jpg"
+  }
+]
